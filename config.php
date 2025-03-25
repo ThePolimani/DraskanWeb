1f330159883d29035340d3cf09c3bc7f65ee0595
Polimani <polimanithev@gmail.com>
2025-03-25 11:11:51 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,20 @@
 <?php
-define('DB_HOST', 'vamrosr800.mysql.db');
-define('DB_USER', 'vamrosr800');
-define('DB_PASS', 'SQLDBbiere64285');
-define('DB_NAME', 'vamrosr800');
+//define('DB_HOST', 'vamrosr800.mysql.db');
+///define('DB_USER', 'vamrosr800');
+//define('DB_PASS', 'SQLDBbiere64285');
+//define('DB_NAME', 'vamrosr800');
+
+$host = 'vamrosr800.mysql.db';
+$dbname = 'vamrosr800';
+$username = 'vamrosr800'; // Change selon ton setup
+$password = 'SQLDBbiere64285'; // Change selon ton setup
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
