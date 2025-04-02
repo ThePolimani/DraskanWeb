31a08bd464d85ed22ecb0ba1a0249e6dbdbc9a4f
Polimani <polimanithev@gmail.com>
2025-04-02 10:57:20 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,4 @@
 <?php
-//define('DB_HOST', 'vamrosr800.mysql.db');
-///define('DB_USER', 'vamrosr800');
-//define('DB_PASS', 'SQLDBbiere64285');
-//define('DB_NAME', 'vamrosr800');
-
 $host = 'vamrosr800.mysql.db';
 $dbname = 'vamrosr800';
 $username = 'vamrosr800'; // Change selon ton setup
