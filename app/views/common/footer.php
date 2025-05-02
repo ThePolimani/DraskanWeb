9de410b53c97def366d31439f5fad395c79a4f8d
Polimani <polimanithev@gmail.com>
2025-05-02 15:20:14 +0200

    tessres

--- app/views/common/footer.php
+++ app/views/common/footer.php
@@ -4,9 +4,14 @@
                 <a href=""><img src="public/images/draskan_texte_logo_blanc.png" alt="Logo Draskan" title="Accueil"></a>
             </div>
             <div id="footer-socials">
-                <a href="#"><img src="public/images/logo_facebook.svg" alt="Facebook"></a>
-                <a href="#"><img src="public/images/logo_insta.svg" alt="Instagram"></a>
-                <a href="#"><img src="public/images/logo_x.svg" alt="Twitter"></a>
+                <div>
+                    <p>Suivez-nous :</p>
+                </div>
+                <div>
+                    <a href="#"><img src="public/images/logo_facebook.svg" alt="Facebook"></a>
+                    <a href="#"><img src="public/images/logo_insta.svg" alt="Instagram"></a>
+                    <a href="#"><img src="public/images/logo_x.svg" alt="Twitter"></a>
+                </div>
             </div>
             <div id="footer-links">
                 <a href="produits.html">Produits</a>
