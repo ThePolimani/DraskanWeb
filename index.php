a42af513bec46bca9ff9b24fbe93fc5b95e22038
Polimani <polimanithev@gmail.com>
2025-05-02 17:04:19 +0200

    gy

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 $pageSpecificCSS = 'accueil.css'; // Changez cette variable selon la page
 
 include_once 'app/views/common/header.php';
-include_once 'app/views/acceuil.php';
+include_once 'app/views/accueil.php';
 include_once 'app/views/common/footer.php';
