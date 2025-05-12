f5fc8e0de9008460b89ccd1d435ecce1206da8ce
Polimani <polimanithev@gmail.com>
2025-05-12 09:44:13 +0200

    Update accueil.php

--- app/views/accueil.php
+++ app/views/accueil.php
@@ -1,6 +1,10 @@
 <main>
     <div id="accueil_1">
-        <h1>DRASKAN</h1>
+        <div>
+            <h1>DRASKAN</h1>
+            <p>Afficher les produits</p>
+        </div>
+        <img src="public/images/bouteille_mockup.png" alt="Bouteille Draskan">
     </div>
     <div id="acceuil_2">
 
