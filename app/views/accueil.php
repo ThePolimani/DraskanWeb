e2f807c702ebea1d51bc2334001caa853f9abe4b
Polimani <polimanithev@gmail.com>
2025-05-12 10:06:42 +0200

    Update accueil.php

--- app/views/accueil.php
+++ app/views/accueil.php
@@ -1,12 +1,25 @@
 <main>
     <div id="accueil_1">
-        <div>
+        <div class="animatedList" data-animation="fadeInLeft" >
             <h1>DRASKAN</h1>
             <p>Afficher les produits</p>
         </div>
-        <img src="public/images/bouteille_mockup.png" alt="Bouteille Draskan">
+        <img src="public/images/bouteille_mockup.png" alt="Bouteille Draskan" class="animated" data-animation="fadeInRight">
     </div>
-    <div id="acceuil_2">
+    <div id="accueil_2">
+        <h2>Nos Produits :</h2>
 
+
+    </div>
+    <div id="accueil_3">
+        <h2>Notre histoire :</h2>
+        <p>Dans une contrée nordique balayée par les vents, les forêts profondes cachent un secret ancestral. Là, 
+            entre les pins imposants, une clairière abrite une source d’eau pure, encadrée de menthe sauvage et de 
+            citronniers rares. La légende raconte qu’un vent particulier, appelé Draskan, traverse ces bois, apportant 
+            fraîcheur et énergie. Un groupe de Vikings, cherchant un breuvage à la hauteur de leur courage et de leurs 
+            terres, s’arrêta dans cette clairière magique. Inspirés par l’arôme des pins, la vivacité du citron vert 
+            et la fraîcheur éclatante de la menthe, ils brassèrent une bière unique. À chaque gorgée de Draskan, 
+            c’est comme si l’on sentait ce vent nordique caresser la peau, éveillant les sens avec sa force et sa fraîcheur.</p>
+        
     </div>
 </main>
