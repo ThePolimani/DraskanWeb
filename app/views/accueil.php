741d347885d93b8eb4c495fdb26312a9cc499a67
Polimani <polimanithev@gmail.com>
2025-05-16 15:01:08 +0200

    Update accueil.php

--- app/views/accueil.php
+++ app/views/accueil.php
@@ -25,9 +25,9 @@
             <img src="public/images/font_toneau.jpg" alt="Image de tonneau" aria-hidden="true" data-animation="fadeInRight" class="animated">
         </div>
     </div>
-    <div id="accueil_4" class="animatedList" data-animation="fadeInLeft" data-reset-delay data-leaf-only>
+    <div id="accueil_4">
         <h2>Nos Partenaires :</h2>
-        <div class="partners">
+        <div class="partners animatedList" data-animation="fadeInLeft" data-reset-delay>
             <?php foreach ($partenaires as $partenaire): ?>
             <div class="partner-card">
                 <img src="<?= htmlspecialchars($partenaire['logo_url']) ?>" alt="Logo <?= htmlspecialchars($partenaire['nom']) ?>" class="partner-logo" />
