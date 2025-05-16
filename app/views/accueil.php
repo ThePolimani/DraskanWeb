61b32d04eff92117811e1eb7480892e2e3c6083b
Polimani <polimanithev@gmail.com>
2025-05-16 09:36:01 +0200

    Update accueil.php

--- app/views/accueil.php
+++ app/views/accueil.php
@@ -11,7 +11,7 @@
 
 
     </div>
-    <div id="accueil_3" class="animatedList" data-animation="fadeInLeft" data-reset-delay data-leaf-only>
+    <div id="accueil_3" class="animatedList" data-animation="fadeInLeft" data-reset-delay>
         <h2>Notre histoire :</h2>
         <div>
             <p>Dans une contrée nordique balayée par les vents, les forêts profondes cachent un secret ancestral. Là, 
