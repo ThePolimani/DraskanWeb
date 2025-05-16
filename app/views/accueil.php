438039e5de65d4cf2aa24d089224d701bb434d1d
Polimani <polimanithev@gmail.com>
2025-05-16 08:53:27 +0200

    Update accueil.php

--- app/views/accueil.php
+++ app/views/accueil.php
@@ -13,13 +13,16 @@
     </div>
     <div id="accueil_3">
         <h2>Notre histoire :</h2>
-        <p>Dans une contrée nordique balayée par les vents, les forêts profondes cachent un secret ancestral. Là, 
-            entre les pins imposants, une clairière abrite une source d’eau pure, encadrée de menthe sauvage et de 
-            citronniers rares. La légende raconte qu’un vent particulier, appelé Draskan, traverse ces bois, apportant 
-            fraîcheur et énergie. Un groupe de Vikings, cherchant un breuvage à la hauteur de leur courage et de leurs 
-            terres, s’arrêta dans cette clairière magique. Inspirés par l’arôme des pins, la vivacité du citron vert 
-            et la fraîcheur éclatante de la menthe, ils brassèrent une bière unique. À chaque gorgée de Draskan, 
-            c’est comme si l’on sentait ce vent nordique caresser la peau, éveillant les sens avec sa force et sa fraîcheur.</p>
-        
+        <div>
+            <p>Dans une contrée nordique balayée par les vents, les forêts profondes cachent un secret ancestral. Là, 
+                entre les pins imposants, une clairière abrite une source d’eau pure, encadrée de menthe sauvage et de 
+                citronniers rares. La légende raconte qu’un vent particulier, appelé Draskan, traverse ces bois, apportant 
+                fraîcheur et énergie. Un groupe de Vikings, cherchant un breuvage à la hauteur de leur courage et de leurs 
+                terres, s’arrêta dans cette clairière magique. Inspirés par l’arôme des pins, la vivacité du citron vert 
+                et la fraîcheur éclatante de la menthe, ils brassèrent une bière unique. À chaque gorgée de Draskan, 
+                c’est comme si l’on sentait ce vent nordique caresser la peau, éveillant les sens avec sa force et sa fraîcheur.
+            </p>
+            <img src="public/images/font_toneau.jpg" alt="Image de tonneau" aria-hidden="true">
+        </div>
     </div>
 </main>
