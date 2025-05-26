6ef2581a03eb5c7f8fbfd909d66e34c5e0bbd599
Polimani <polimanithev@gmail.com>
2025-05-26 14:18:54 +0200

    1

---
+++ newsletter2.php
@@ -0,0 +1,5 @@
+<?php
+$pageSpecificCSS = 'newsletter.css'; // Changez cette variable selon la page
+include_once 'app/views/common/header.php';
+include_once 'app/views/newsletter.php';
+include_once 'app/views/common/footer.php';
