3d172b74ad3be31099e33b8ed82ce39d5abe16f1
Polimani <polimanithev@gmail.com>
2025-06-04 11:47:58 +0200

    1

--- app/views/common/header.php
+++ app/views/common/header.php
@@ -23,7 +23,8 @@
     <?php if (!empty($pageSpecificJS)): ?>
         <script src="public/scripts/<?= $pageSpecificJS ?>"></script>
     <?php endif; ?>
-	<!--<link rel="apple-touch-icon" href="/public/images/app-icon.png">-->
+	<link rel="apple-touch-icon" href="/public/images/app-icon.png">
+    <meta property="og:image" content="/public/images/fav-image.png">
 </head>
 <body>
     <div id="ageVerificationModal">
