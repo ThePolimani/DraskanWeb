8445aaa92f4cf8efb97fe80172b6421629a03a02
Polimani <polimanithev@gmail.com>
2025-06-10 18:29:19 +0200

    Update footer.php

--- app/views/common/footer.php
+++ app/views/common/footer.php
@@ -1,7 +1,7 @@
 <footer>
         <div id="footer-content" class="animatedList" data-animation="fadeIn" data-reset-delay>
             <div id="footer-logo">
-                <a href=""><img src="public/images/draskan_texte_logo_blanc.png" alt="Logo Draskan" title="Accueil"></a>
+                <a href="/"><img src="public/images/draskan_texte_logo_blanc.png" alt="Logo Draskan" title="Accueil"></a>
             </div>
             <div id="footer-socials">
                 <div>
@@ -14,14 +14,14 @@
                 </div>
             </div>
             <div id="footer-links">
-                <a href="produits" id="link_produits">Produits</a>
-                <a href="partenaires" id="link_partenaires">Partenaires</a>
+                <a href="/produits" id="link_produits">Produits</a>
+                <a href="/partenaires" id="link_partenaires">Partenaires</a>
                 <a href="/apropos" id="link_apropos">A Propos</a>
             </div>
             <div id="footer-legal-links">
-                <a href="contact" id="link_contact">Nous Contactez</a>
-                <a href="mentions-legales" id="link_mentionlegal">Mentions légales</a>
-                <a href="conditions-utilisation" id="link_conditions">Conditions d'utilisation</a>
+                <a href="/contact" id="link_contact">Nous Contactez</a>
+                <a href="/mentions-legales" id="link_mentionlegal">Mentions légales</a>
+                <a href="/conditions-utilisation" id="link_conditions">Conditions d'utilisation</a>
             </div>
         </div>
         <div id="footer-legal" class="animatedList" data-animation="fadeIn">
